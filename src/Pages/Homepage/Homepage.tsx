b0c9b29a177a0bdb1db3538a4154ae4c3fd49681
olubiyi babajide <olubiyibabajide@gmail.com>
2025-09-05 13:24:33 +0100

    mobile responsiveness continued

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -1,9 +1,10 @@
 import underconstruction from "../../assets/images/img.jpg";
+import "./Homepage.css"
 
 export const Homepage = () => {
   return (
-    <div>
-      <img src={underconstruction} alt="" />
+    <div className="homepage">
+      <img src={underconstruction} alt="img" />
     </div>
   );
 };
